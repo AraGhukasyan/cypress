b1db5490337385d16dd5b9279f2f3b950506664d
Ara Ghukasyan <153002187+AraGhukasyan@users.noreply.github.com>
2024-01-20 00:43:54 +0400

    Update cypressHomeWork.cy.js
    In the previous version of this code, I forgot to include assertions. I hope this version is correct now.

--- cypressHomeWork.cy.js
+++ cypressHomeWork.cy.js
@@ -1,23 +1,45 @@
-/// <reference types="cypress" />
+// <reference types="cypress" />
 
 describe('Shopping Cart and Order Test', () => {
   it('should add a product to the cart and make an order', () => {
-    // visit the website 
+    // Visit the website 
     cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
-    //click the + button 
-    cy.get('#root > div > div.products-wrapper > div > div:nth-child(6) > div.stepper-input > a.increment').click()
-    //click the "ADD TO CART"
-    cy.get('#root > div > div.products-wrapper > div > div:nth-child(6) > div.product-action > button').click()
-    
-     cy.get('.cart-icon img').click();
+
+    // Click the + button to increase the quantity
+    cy.get('#root > div > div.products-wrapper > div > div:nth-child(6) > div.stepper-input > a.increment').click();
+
+    // Click the "ADD TO CART" button
+    cy.get('#root > div > div.products-wrapper > div > div:nth-child(6) > div.product-action > button').click();
+
+    // Assert that the cart icon is visible
+    cy.get('.cart-icon img').should('be.visible');
+
+    // Click on the cart icon
+    cy.get('.cart-icon img').click();
+
+    // Assert that the "PROCEED TO CHECKOUT" button is visible and clickable
+    cy.contains('PROCEED TO CHECKOUT').should('be.visible', 'not.be.disabled');
+
+    // Click on the "PROCEED TO CHECKOUT" button
     cy.contains('PROCEED TO CHECKOUT').click();
 
-     // Click on "Place Order" button
-     cy.contains('Place Order').click();
+    // Assert that the "Place Order" button is visible and clickable
+    cy.contains('Place Order').should('be.visible', 'not.be.disabled');
+
+    // Click on the "Place Order" button
+    cy.contains('Place Order').click();
 
     // Fill in the form and complete the order
-     cy.get('#root > div > div > div > div > div > select').select("Armenia");
-    cy.get('#root > div > div > div > div > input').click();
-    cy.get('#root > div > div > div > div > button').click();
-   });
+    // Assert that the country select dropdown is visible
+    cy.get('#root > div > div > div > div > div > select').should('be.visible');
+
+    // Select a country from the dropdown
+    cy.get('#root > div > div > div > div > div > select').select("Armenia");
+
+    // Assert that the terms and conditions checkbox is visible and check it
+    cy.get('#root > div > div > div > div > input').should('be.visible', 'not.be.disabled').check();
+
+    // Assert that the "Proceed" button is visible and click it
+    cy.get('#root > div > div > div > div > button').should('be.visible').click();
+  });
 }); 
